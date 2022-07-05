615748f5243a6fd1520bad5c395a8f452c4c7076
B1tbuling <polzikov0510@gmail.com>
2022-07-06 01:45:21 +0300

    Remove comments :(

--- posts/forms.py
+++ posts/forms.py
@@ -3,13 +3,6 @@ from .models import *
 from django.core.exceptions import ValidationError
 
 class TagForm(forms.ModelForm):
-
-    # title = forms.CharField(max_length=50)
-    # slug = forms.CharField(max_length=50)
-    #
-    # title.widget.attrs.update({'class': 'form-control'})
-    # slug.widget.attrs.update({'class': 'form-control'})
-
     class Meta:
         model = Tag
         fields = ['title', 'slug']
@@ -29,13 +22,6 @@ class TagForm(forms.ModelForm):
             raise ValidationError('Slug must be unique. We have a "{}" slug already'.format(new_slug))
         return new_slug
 
-
-    # def save(self):
-    #     new_tag = Tag.objects.create(
-    #         title=self.cleaned_data['title'],
-    #         slug=self.cleaned_data['slug']
-    #     )
-    #     return new_tag
 
 class PostForm(forms.ModelForm):
     class Meta:
--- posts/views.py
+++ posts/views.py
@@ -1,39 +1,22 @@
-from django.shortcuts import render, redirect
-from django.shortcuts import get_object_or_404
 from django.views.generic import View
 
-from .models import Post, Tag
 from .utils import *
 from .forms import TagForm, PostForm
 
-from django.http import HttpResponse, Http404
 
 class PostsList(ObjectsListMixin, View):
-
     model = Post
     template = 'blog/index.html'
 
-    # def get(self, request):
-    #     posts = Post.objects.all()
-    #     return render(request, 'blog/index.html', context={'posts': posts})
-
 
 class TagsList(ObjectsListMixin, View):
-
     model = Tag
     template = 'blog/tags_list.html'
-
-    # def get(self, request):
-    #     tags = Tag.objects.all()
-    #     return render(request, 'blog/tags_list.html', context={'tags': tags })
 
 
 class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
-    # def get(self, request, slug):
-    #     post = get_object_or_404(Post, slug__iexact=slug)
-    #     return render(request, 'blog/post_detail.html', context={'post': post})
 
 
 class PostCreate(ObjectCreateMixin, View):
@@ -44,29 +27,8 @@ class PostCreate(ObjectCreateMixin, View):
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
-    # def get(self, request, slug):
-    #     tag = get_object_or_404(Tag, slug__iexact=slug)
-    #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
-
-        # Аналог записи
-        # tags = Tag.objects.filter(slug__iexact=slug)
-        # if len(tags) > 0:
-        #     tag = tags[0]
-        #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
-        # else:
-        #     raise Http404()
 
 
 class TagCreate(ObjectCreateMixin, View):
     model_form = TagForm
     template = 'blog/tag_create.html'
-
-# def tag_detail(request, slug):
-#     tag = Tag.objects.get(slug__iexact=slug)
-#     return render(request, 'blog/tag_detail.html', context={'tag': tag})
-
-# def post_detail(request, slug):
-#     post = Post.objects.get(slug__iexact=slug)
-#     return render(request,'blog/post_detail.html', context={'post': post})
-
-
